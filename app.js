e54392627de50a1ebff060cfe36325b36704e982
Mohammed Akram <nerddude9000@proton.me>
2026-08-17 19:04:08 +0100

    setup: app.js, prisma and passport

---
+++ app.js
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import express from "express";
+import path from "path";
+import { setup_passport } from "./setup-passport.js";
+import expressSession from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { prisma } from "./lib/prisma.js";
+import passport from "passport";
+
+const app = express();
+
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
+
+setup_passport();
+
+app.use(express.static(path.join(import.meta.dirname, "public")));
+app.use(
+  expressSession({
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 5 * 60 * 1000, // 5 mins
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 7 days
+  }),
+);
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
+
+// set locals.user, this is mainly for ejs
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log(`Minidrive running at port ${PORT}`);
+});
---
+++ setup-passport.js
@@ -0,0 +1,46 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { prisma } from "./lib/prisma.js";
+
+export function setup_passport() {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "username" },
+      async (username, password, done) => {
+        try {
+          const user = await prisma.user.findUnique({
+            where: { username },
+          });
+
+          if (!user) {
+            return done(null, false, {
+              message: "Incorrect username or it doesn't exist",
+            });
+          }
+
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch) {
+            return done(null, false, { message: "Incorrect password" });
+          }
+
+          done(null, user);
+        } catch (err) {
+          done(err);
+        }
+      },
+    ),
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = prisma.user.findUnique({ where: { id } });
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+}
